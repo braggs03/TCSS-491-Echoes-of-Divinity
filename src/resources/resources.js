8c79818bfe6c80df85da3257f4b733970d38ce68
Brandon <brandonragghianti@gmail.com>
2025-01-29 17:22:13 -0800

    Removed duplicate entities constants.

--- src/resources/resources.js
+++ src/resources/resources.js
@@ -4,14 +4,11 @@ const MECHA_GOLEM = './src/resources/MechaGolem.png'
 const KNIGHT = './resources/knight/'
 const SKELETON_WARRIOR = './resources/skeletonwarrior/'
 const KNIGHT_SPRITE = './resources/knight/knightsprite.png'
-
 const GORGON = './resources/Gorgon/Gorgon'
 const CELES = './resources/Celes/Celes'
 
 // World
 
 const DUNGEON = './resources/dungeon.png'
-const GORGON = './resources/Gorgon/Gorgon'
-const CELES = './resources/Celes/Celes'
 const TORCH = './resources/torch.png'
 const DUNGEON_BACKGROUND_IMAGE = './resources/dungeonbackground.png'
