a6ea5fa7cd60845d2b6b3c79f5d80d8fba3f24fd
hamdajama <hamda.jama2525@gmail.com>
2025-02-13 17:58:19 -0800

    Update levels.js

--- src/levels.js
+++ src/levels.js
@@ -58,9 +58,25 @@ const levels = {
         dungeonGround: [{ x: 0, y: 17, w: 40, h: 1 }, { x: 0, y: 0, w: 40, h: 1 },{ x: 20, y: 11, w: 1, h: 1 }],
         mechagolem: [{ x: 900, y: 350 }],
         dungeonWall: [{ x: 0, y: 0, h: 5 }],
-        dungeonDoor: [{ x: 117, y: 465, level: "shopkeeper" }],
+        dungeonDoor: [{ x: 117, y: 465, level: "two" }],
         bonFire: [{ x: 400, y: 465, level: "one" }],
         dungeonBackground2: [{ x:0, y:0, w:50, h: 1}],
+    },
+
+    two: {
+        width: 10000,
+        height: 0,
+        knightPos: {
+            x: 87,
+            y: 320,
+        },
+        dungeonBackground3: [{ x:0, y:0, w:50, h: 1}],
+        dungeonGround2: [{ x: 0, y: 14.5, w: 6, h: 1 }, {x:7, y: 14.5, w: 8, h:1},{ x: 16, y: 14.5, w: 8, h: 1 },{ x:25, y: 14.5, w: 10, h: 1 }, {x: 36, y: 14.5, w: 8, h: 1}, {x: 45, y: 14.5, w: 8, h: 1}, {x: 54, y: 14.5, w: 10, h: 1}, { x: 0, y: 0, w: 100, h: 1 }],
+       //mechagolem: [{ x: 950, y: 270 }],
+        dungeonWall: [{ x: 0, y: 0, h: 4 }],
+        dungeonWaterfall: [{x: 950, y: 219}, { x: 2345, y: 219}, {x:3735, y: 219}, {x: 5445, y:219}, {x: 6835, y: 219}, {x: 8235, y: 219}],
+        dungeonDoor: [{ x: 124, y: 360, level: "shopkeeper" }],
+
     },
 
     shopkeeper: {
@@ -90,6 +106,7 @@ const levels = {
         azucena: [{ x: 870, y: 510 }],
         tent: [{ x: 700, y: 478 }],
         bonFire: [{ x: 400, y: 465, level: "shopkeeper" }],
+        potion: [{ x: 788, y: 570}, { x: 888, y: 570}, { x: 990, y: 570}],
     },
 
     bossroom: {
